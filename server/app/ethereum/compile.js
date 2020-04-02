571c4ebdcda251f7f2dc50ebe75f4928053e8459
Levysantiago <levyssantiago@gmail.com>
2020-04-02 20:05:10 -0300

    removing env

--- server/app/ethereum/compile.js
+++ server/app/ethereum/compile.js
@@ -1,13 +1,10 @@
-require("dotenv").config();
-
-const env = process.env;
 const path = require("path");
 const fs = require("fs-extra");
 const solc = require("solc");
 const buildPath = path.resolve(__dirname, "build");
 fs.removeSync(buildPath);
 
-const contractPath = path.resolve(__dirname, "contracts", env.SMC_NAME);
+const contractPath = path.resolve(__dirname, "contracts", "dht11.sol");
 const source = fs.readFileSync(contractPath, "UTF-8");
 const output = solc.compile(source, 1).contracts;
 fs.ensureDirSync(buildPath);
