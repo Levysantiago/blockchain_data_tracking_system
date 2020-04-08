278444b0774c257414d20df28144845d06310ad6
Levysantiago <levyssantiago@gmail.com>
2020-04-08 19:26:30 -0300

    adding more SMC functions calls to ContractService

--- server/app/services/ContractService.js
+++ server/app/services/ContractService.js
@@ -1,5 +1,3 @@
-const Web3 = use("web3");
-
 class ContractService {
   constructor(web3, contractInstance) {
     this.web3 = web3;
@@ -12,9 +10,36 @@ class ContractService {
       .send({ from: from, gas: gas });
   }
 
+  async setHumidity(humidity, from, gas) {
+    await this.contract.methods
+      .setHumidity(temperature)
+      .send({ from: from, gas: gas });
+  }
+
+  async setTemperatureAndHumidity(measures, from, gas) {
+    if (measures.length > 1) {
+      await this.contract.methods
+        .setTemperature(measures[0])
+        .send({ from: from, gas: gas });
+      await this.contract.methods
+        .setHumidity(measures[1])
+        .send({ from: from, gas: gas });
+    }
+  }
+
   async getTemperatures() {
     const temperatures = await this.contract.methods.getTemperatures().call();
     return temperatures;
+  }
+
+  async getHumidities() {
+    const humidities = await this.contract.methods.getHumidities().call();
+    return humidities;
+  }
+
+  async getLastMeasures() {
+    const last_measures = await this.contract.methods.getLastValues().call();
+    return last_measures;
   }
 }
 
