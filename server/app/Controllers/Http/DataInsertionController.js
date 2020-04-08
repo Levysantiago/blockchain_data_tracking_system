e9f2b3079b5028bd216385c41abf3f8c9b4a7947
Levysantiago <levyssantiago@gmail.com>
2020-04-08 19:23:45 -0300

    server adding measurements to SMC

--- server/app/Controllers/Http/DataInsertionController.js
+++ server/app/Controllers/Http/DataInsertionController.js
@@ -1,15 +1,17 @@
 "use strict";
 
-const Web3 = require("web3");
 require("dotenv").config();
-const HDWalletProvider = require("truffle-hdwallet-provider");
 const {
   MNEMONIC_WORDS,
   PROVIDER_LINK,
   SMC_ADDRESS,
+  BC_ADDRESS,
   SMC_BUILD_NAME,
   GAS
 } = process.env;
+
+const Web3 = require("web3");
+const HDWalletProvider = require("truffle-hdwallet-provider");
 const provider = new HDWalletProvider(MNEMONIC_WORDS, PROVIDER_LINK);
 const web3 = new Web3(provider);
 const web3bridge = use("App/lib/web3bridge");
@@ -23,24 +25,28 @@ class DataInsertionController {
    * JSON EXAMPLE
    * {
    *    "sensorname": "DHT11",
-   *    "contractAddress": "0x18eF2C83F4CB2FFB546d630AEbA2a4C324d8fDb7",
    *    "measurements":[5, 6]
    * }
    *
-   * @param {object} ctx
-   * @param {Request} ctx.req
-   * @param {Response} ctx.res
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
    */
-  async getSensorMeasurement({ request, response }) {
+  async setMeasurement({ request, response }) {
     const contract = await web3bridge.getContractInstance(
       SMC_ADDRESS,
       SMC_BUILD_NAME,
       web3
     );
-    const contract_service = new ContractService(web3, contract);
-    const temperatures = await contract_service.getTemperatures();
+    const { measurements } = request.post();
 
-    response.send(JSON.stringify(temperatures));
+    const contract_service = new ContractService(web3, contract);
+    await contract_service.setTemperatureAndHumidity(
+      measurements,
+      BC_ADDRESS,
+      GAS
+    );
+
+    response.send(200);
   }
 }
 
